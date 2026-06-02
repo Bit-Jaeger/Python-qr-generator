5e59121781662ccc8736a06fc3450fd707f7f8a1
Bit-Jaeger <147092976+Bit-Jaeger@users.noreply.github.com>
2026-06-02 22:42:30 +0200

    getting user Input: should code be customized?

--- qr.py
+++ qr.py
@@ -9,15 +9,43 @@ qr = qrcode.QRCode(
 )
 
 
+print(f"Do you want to customize size and color from your qr code? [Y/N]")
+def prompt_custom_yn()
+
+
+
+# prompt user if code should be customized - color,border, ...
+def prompt_custom_yn(){
+
+    doCustom = input()
+    if(doCustom.lower() == "n"){
+        basic_qr()
+    }
+    elif(doCustom.lower() == "y"){
+        custom_qr()
+    }
+    else{
+        print(f"Please provide a valid answer: [Y/N]")
+    }
+}
 
 
 
 
 
-qr.add_data('Some data')
-qr.make(fit=True)
+
+def basic_qr(){
+    print(f"This will be the basic qr-code")
+}
+
+def custom_qr(){
+    print(f"Custom QR-Code will be generated.")
+}
+
+#qr.add_data('Some data')
+#qr.make(fit=True)
 
 
 
 
-img = qr.make_image(fill_color="black", back_color="white")
+#img = qr.make_image(fill_color="black", back_color="white")
