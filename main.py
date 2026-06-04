4484bbe823a5afd1db7bf00666fa265e4b6fb057
Bit-Jaeger <147092976+Bit-Jaeger@users.noreply.github.com>
2026-06-04 19:17:42 +0200

    removing test button, starting with basic ctk layout: label, entry, checkbox1

--- main.py
+++ main.py
@@ -10,31 +10,63 @@ qr = qrcode.QRCode(
     border=4,
 )
 
+class Label_Frame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.label = ctk.CTkLabel(self, text="Jaeger's QR-Code Generator", font=ctk.CTkFont(size=20, weight="bold"))
+        self.label.grid(row=0, column=0, padx=20, pady=25, sticky="ew", columnspan=2)
+
+class Entry_Frame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.entry = ctk.CTkEntry(self, placeholder_text="Paste URL here")
+        self.entry.grid(row=1, column=0, padx= 20, pady=20, sticky="ew", columnspan=1)
+
+
+class Options_Frame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        check_var = ctk.StringVar(value="off")
+        self.checkbox = ctk.CTkCheckBox(self, text="Customize your QR?", command=allow_custom_qr, variable=check_var, onvalue="on", offvalue="off")
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("QR-Code Generator")
+        self.geometry("700x400")
+        self.grid_columnconfigure((0, 1), weight=1)
+        
+        # Creating the Label frame off of parent class
+        self.label_frame = Label_Frame(self)
+        self.label_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew", columnspan=2)
+
+        self.entry_frame = Entry_Frame(self)
+        self.entry_frame.grid(row=1, column=0, padx=20, pady= 20, sticky="nsew")
+
+        self.customqr_frame = Options_Frame(self)
+        self.customqr_frame.grid(row=1, column= 1, padx=20, pady=20, sticky="nw")
+
+
 
 
 
 def main():
     print(f"Welcome to the QR-Code Generator!")
+
+
+    app = App()
+    app.mainloop()
     prompt_custom_yn()
 
-    #generate the UI 
-    app = ctk.CTk()
-    app.geometry("500x400")
-
-    #config for Button
-    button = ctk.CTkButton(app, text="Generate QR-Code", command=button_test)
-    button.grid(row=0, column=0, padx=20, pady=20)
 
 
-
-    app.mainloop()
-
-
-
-
-
-def button_test():
-    print(f"Button was pressed")
+def allow_custom_qr():
+    #TODO: allow checkboxes/inputs for customizable qr-code (color, ...)
+    return 0
 
 # prompt user if code should be customized
 def prompt_custom_yn():
