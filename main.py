89786fcefdc03626c9873a7a955c6c8abdc29f14
Bit-Jaeger <147092976+Bit-Jaeger@users.noreply.github.com>
2026-06-04 13:31:32 +0200

    creating first button with ctk, fixing errors for multiple promts

--- main.py
+++ main.py
@@ -12,49 +12,56 @@ qr = qrcode.QRCode(
 
 
 
+
 def main():
     print(f"Welcome to the QR-Code Generator!")
-    print(f"Do you want to customize size and color from your qr code? [Y/N]")
     prompt_custom_yn()
 
+    #generate the UI 
+    app = ctk.CTk()
+    app.geometry("500x400")
+
+    #config for Button
+    button = ctk.CTkButton(app, text="Generate QR-Code", command=button_test)
+    button.grid(row=0, column=0, padx=20, pady=20)
+
+
+
+    app.mainloop()
 
 
 
 
-    # prompt user if code should be customized - color,border, ...
-    def prompt_custom_yn():
 
-        doCustom = input()
-        if doCustom.lower() == "n":
-            basic_qr()
-        
-        elif doCustom.lower() == "y":
-            custom_qr()
-        
-        else:
-            sys.exit("Please provide a valid answer: [Y/N]")
+def button_test():
+    print(f"Button was pressed")
 
-        # decide what function to call, when user decided
-
-        
-
-    def basic_qr():
-        print(f"This will be the basic qr-code")
-        return 0
-
-
-    def custom_qr():
-        print(f"Custom QR-Code will be generated.")
-        return 0
-
-
-
+# prompt user if code should be customized
+def prompt_custom_yn():
 
     print(f"Do you want to customize size and color from your qr code? [Y/N]")
-    prompt_custom_yn()
+    doCustom = input()
+    if doCustom.lower() == "n":
+        basic_qr()
+    
+    elif doCustom.lower() == "y":
+        custom_qr() 
+    
+    else:
+        sys.exit("Please provide a valid answer: [Y/N]")
+
+    # decide what function to call, when user decided
+
+    
+
+def basic_qr():
+    print(f"This will be the basic qr-code")
+    return 0
 
 
-
+def custom_qr():
+    print(f"Custom QR-Code will be generated.")
+    return 0
 
 
 
