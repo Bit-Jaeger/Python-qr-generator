bc24dbad535b30244b50c604a6b650d1af95f757
Bit-Jaeger <147092976+Bit-Jaeger@users.noreply.github.com>
2026-06-07 17:20:37 +0200

    implementing passing entry value to basic_qr() via all objects.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import qrcode
 import customtkinter as ctk
 
-
+# ------ GENERATING LOGIC -------
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -9,6 +9,10 @@ qr = qrcode.QRCode(
     border=4,
 )
 
+
+# ------ UI LOGIC -------
+
+# ______ Contents ________
 #Label should be displayed at all times
 class Label_Frame(ctk.CTkFrame):
     def __init__(self, master):
@@ -17,24 +21,16 @@ class Label_Frame(ctk.CTkFrame):
         self.label = ctk.CTkLabel(self, text="Jaeger's QR-Code Generator", font=ctk.CTkFont(size=20, weight="bold"), corner_radius = 50)
         self.label.grid(row=0, column=0, padx=20, pady=25, sticky="ew", columnspan=2)
 
-
 #Creating segmented button used as main navigator
 class Segmented_Button_Frame(ctk.CTkFrame):
     def __init__(self, master):
-        super().__init__(master)
+        super().__init__(master, corner_radius=50)
         self.seg_button = ctk.CTkSegmentedButton(self, corner_radius=50, values=["Entry", "Customize"], border_width=5, height=50, command=self.seg_btn_callback)
         self.seg_button.grid(row=1, column=1, sticky="n")
         self.seg_button.set("Entry")
 
     def seg_btn_callback(self, value):
         print(f"{value}")
-
-
-
-
-
-
-
 
 #Seg_Button is on QR-Code
 class Entry_Frame(ctk.CTkFrame):
@@ -43,28 +39,23 @@ class Entry_Frame(ctk.CTkFrame):
 
         self.entry = ctk.CTkEntry(self, placeholder_text="Paste URL here")
         self.entry.grid(row=2, column=0, padx= 20, pady=20, sticky="n", columnspan=3)
+    def get_input_url(self):
+        print("get_input_url is being called!!")
+        return self.entry.get()
 
 
 class Btn_GenQR_Frame(ctk.CTkFrame):
-    def __init__(self, master):
+    def __init__(self, master, event_generate):
         super().__init__(master, corner_radius=50)
 
-        self.generate_btn = ctk.CTkButton(self, text="Generate QR")
+        self.generate_btn = ctk.CTkButton(self, text="Generate QR", command=event_generate)
         self.generate_btn.grid(row=2, column=1, padx=20, pady=20, sticky="n")
-
-    def generate_qr():
-        print(f"Generating the QR-Code => Logic has to go here")
-
+        def event_generate():
+            super().pass_qr()
 
 
 
-
-
-
-
-
-
-#Class creating the FRAMES
+# ______ Frames ________
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -77,21 +68,20 @@ class App(ctk.CTk):
         self.label_frame = Label_Frame(self)
         self.label_frame.grid(row=0, column=1, padx=20, pady=30, sticky="n", columnspan=1)
 
-
-
         #Creating seg_btn
         self.seg_btn = Segmented_Button_Frame(self)
         self.seg_btn.grid(row=1, column=1, padx=20, pady=20, sticky="n")
 
+        self.entry = Entry_Frame(self)
+        self.entry.grid(row=2, column=1, padx=20, pady=20, sticky="n")
 
-
-
-
-        self.entry_frame = Entry_Frame(self)
-        self.entry_frame.grid(row=2, column=1, padx=20, pady=20, sticky="n")
-
-        self.genQR_frame = Btn_GenQR_Frame(self)
-        self.genQR_frame.grid(row=3, column=1, padx=20, pady=20, sticky="n")
+        self.genQR = Btn_GenQR_Frame(self, self.pass_qr)
+        self.genQR.grid(row=3, column=1, padx=20, pady=20, sticky="n")
+        
+        
+    def pass_qr(self):
+        url = self.entry.get_input_url()
+        basic_qr(url)
 
 
 
@@ -105,24 +95,28 @@ class App(ctk.CTk):
 
 
 def main():
-    print(f"Welcome to the QR-Code Generator!")
 
     app = App()
+ 
     app.mainloop()
 
+   
+        
+    #current_input = app.entry.get_input_url()
+    #basic_qr(current_input)
+    
+    
+    
 
 
-
-
-
-def basic_qr():
+def basic_qr(url):
     print(f"This will be the basic qr-code")
-    return 0
+    print(f"basic_qr functino gets value:{url}")
 
 
-def custom_qr():
-    print(f"Custom QR-Code will be generated.")
-    return 0
+
+
+
 
 
 
